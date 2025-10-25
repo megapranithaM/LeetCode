672f056a86e925a973094642bbbc6920068a4354
MEGAPRANITHAA M <155057431+megapranithaM@users.noreply.github.com>
2025-10-25 17:02:17 +0530

    Time: 0 ms (100%), Space: 40.1 MB (91.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0 || n == 1) {
+        if (n==0||n==1){
             return n;
         }
-
-        int prev = 0;
-        int curr = 1;
-        int next = 0;
-
-        for (int i = 2; i <= n; i++) {
-            next = prev + curr;  
-            prev = curr;         
-            curr = next;         
+        int prev=0;
+        int curr=1;
+        int next=0;
+        for(int i=2;i<=n;i++){
+            next=prev+curr;
+            prev=curr;
+            curr=next;
         }
-
         return curr;
     }
 }
