0af858041819b170d37f92f5a98f1f2e88989c4e
MEGAPRANITHAA M <155057431+megapranithaM@users.noreply.github.com>
2025-10-25 20:34:16 +0530

    Time: 1 ms (81.4%), Space: 41.6 MB (5.36%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int temp=0;
+        int rem=0;
+        while(x!=0){
+            if(temp>Integer.MAX_VALUE/10||temp<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            rem=x%10;
+            temp=temp*10+rem;
+            x/=10;
+        }
+        return temp;
+    }
+}
