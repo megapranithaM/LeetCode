0909ade960ca8fc5961790540431a512598f5a90
MEGAPRANITHAA M <155057431+megapranithaM@users.noreply.github.com>
2025-12-12 21:52:34 +0530

    Time: 0 ms (100%), Space: 10.2 MB (37.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,12 +1,12 @@
 int majorityElement(int* nums, int n) 
 {
-    int count = 0;
-    int candidate = 0;
-    for (int i = 0; i < n; i++) {
-        if (count == 0) {
-            candidate = nums[i];
+    int count=0;
+    int candidate=0;
+    for(int i=0;i< n;i++) {
+        if(count==0){
+            candidate=nums[i];
         }
-        count += (nums[i] == candidate) ? 1 : -1;
+        count +=(nums[i]==candidate)?1:-1;
     }
     return candidate;
 
