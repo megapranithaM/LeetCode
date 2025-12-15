42bc885525699f85fcda7021bd94b948b3b9776b
MEGAPRANITHAA M <155057431+megapranithaM@users.noreply.github.com>
2025-12-15 21:56:42 +0530

    Time: 317 ms (5.32%), Space: 47.4 MB (38.5%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,16 @@
-class Solution{
-    public int pivotIndex(int[] nums){
+class Solution {
+    public int pivotIndex(int[] nums) {
         for(int i=0;i<nums.length;i++){
-            int left=0;
-            int right=0;
+            int left=0,right=0;
             for(int j=0;j<i;j++){
                 left+=nums[j];
-        }
+            }
             for(int j=i+1;j<nums.length;j++){
                 right+=nums[j];
-        }
-            if(left==right){
+            }
+            if(right==left){
                 return i;
-                }
-
+            }
         }
         return -1;
     }
