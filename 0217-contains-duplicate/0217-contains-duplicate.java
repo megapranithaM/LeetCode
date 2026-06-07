20b747dc992a4d0a407fba749d0faf5a5b8f3175
MEGAPRANITHAA M <155057431+megapranithaM@users.noreply.github.com>
2026-06-07 19:06:01 +0530

    Time: 14 ms (88.89%), Space: 93.6 MB (40.83%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums); 
-        for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i+1]) {
-                    return true;
-                }
+      HashSet<Integer> set=new HashSet<>();
+      for(int num:nums){
+        if(set.contains(num)){
+            return true;
+        }else{
+            set.add(num);
         }
-        return false;
+       
+      }
+      return false;
     }
 }
 
