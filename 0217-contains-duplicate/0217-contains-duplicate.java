bbc9f6564e312e8c71dbac459906f494dd2acc9d
MEGAPRANITHAA M <155057431+megapranithaM@users.noreply.github.com>
2026-06-27 15:57:44 +0530

    Time: 18 ms (32.9%), Space: 108.2 MB (6.09%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,9 +4,9 @@ class Solution {
         for(int num:nums){
             if(set.contains(num)){
                 return true;
+            }else{
+                set.add(num);
             }
-            set.add(num);
-        
         }
         return false;
     }
