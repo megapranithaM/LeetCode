5aab3a192b3bfd907dc7566e70e1b76fcb77565d
MEGAPRANITHAA M <155057431+megapranithaM@users.noreply.github.com>
2026-07-12 18:47:28 +0530

    Time: 2 ms (7.89%), Space: 80.5 MB (22.62%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int max = arr[0];
-        int index = 0;
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                index = i;
+        int max=arr[0];
+        int index=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                index=i;
             }
         }
-
         return index;
     }
 }
